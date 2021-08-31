aa2bb73d561b37e2263c6fac2b6b0b3054d65c70
Dmitry <upssq@mail.ru>
2021-08-31 18:04:59 +0300

    Доавил js

--- source/js/script.js
+++ source/js/script.js
@@ -1,22 +1,28 @@
 /* eslint-disable no-shadow */
 /* eslint-disable id-length */
+const program = document.querySelector('.program');
+const programButtons = program.querySelectorAll('.program__item-button');
+const programItems = program.querySelectorAll('.program__item');
+
 const life = document.querySelector('.life');
-const lifeElement = life.querySelectorAll('.life__item');
+const lifeElements = life.querySelectorAll('.life__item');
 const lifeButtons = life.querySelectorAll('.life__item-button');
 
 const switchSlides = (switchers, slides) => {
   for (let i = 0; i < switchers.length; i++) {
-    switchers[i].addEventListener('click', () => {
+    switchers[i].addEventListener('click', (evt) => {
+      evt.preventDefault();
 
       for (let i = 0; i < switchers.length; i++) {
-        switchers[i].classList.remove('life__item-button--active');
-        slides[i].classList.remove('life__item--active');
+        switchers[i].classList.remove('active');
+        slides[i].classList.remove('active');
       }
 
-      switchers[i].classList.add('life__item-button--active');
-      slides[i].classList.add('life__item--active');
+      switchers[i].classList.add('active');
+      slides[i].classList.add('active');
     });
   }
 };
 
-switchSlides(lifeButtons, lifeElement);
+switchSlides(programButtons, programItems);
+switchSlides(lifeButtons, lifeElements);
