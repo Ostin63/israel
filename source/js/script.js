5937274b6b39f144ed2f193c4a03a1ad642f8884
Dmitry <upssq@mail.ru>
2021-09-03 15:39:01 +0300

    Стили для details

--- source/js/script.js
+++ source/js/script.js
@@ -9,6 +9,7 @@ const programItems = program.querySelectorAll('.program__item');
 const life = document.querySelector('.life');
 const lifeElements = life.querySelectorAll('.life__item');
 const lifeButtons = life.querySelectorAll('.life__item-button');
+
 const faqItems = document.querySelectorAll('.faq__item');
 
 const switchSlides = (switchers, slides) => {
@@ -53,3 +54,34 @@ new Swiper('.swiper-container', {
     type: 'fraction',
   },
 });
+
+const map = L.map('map')
+  .setView({
+    lat: 55.0285,
+    lng: 82.9281,
+  }, 18);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: 'img/marker-map.png',
+  iconSize: [26, 40],
+  iconAnchor: [13, 60],
+});
+
+const mainPinMarker = L.marker(
+  {
+    lat: 55.0285,
+    lng: 82.9281,
+  },
+  {
+    icon: mainPinIcon,
+  },
+);
+
+mainPinMarker.addTo(map);
